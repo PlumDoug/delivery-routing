94320906e8a5f45220326037e1398dea06e61b8f
Doug Faehndrich <dfaehn@gmail.com>
2025-06-09 17:55:21 -0400

    Creates file structure, sets up venv, creates some files.

---
+++ main.py
@@ -0,0 +1,5 @@
+def main():
+    print("Running Program...")
+
+if __name__ == "__main__":
+    main()
---
+++ src/HashTable.py
@@ -0,0 +1,16 @@
+class HashTable():
+
+    def __init__(self, size=10):
+        self.size = size
+        self.buckets = []
+        for i in range(size):
+            self.buckets.append([])
+
+    
+
+    
+
+
+
+
+   
