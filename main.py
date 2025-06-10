da66da925efc7ab7e820c512125ff055012d17ad
Doug Faehndrich <dfaehn@gmail.com>
2025-06-09 22:08:25 -0400

    Removes unnecessary class Package, loads data from csv and enters data into hash table in main.py.

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+from src.data_loader import load_package_data
+from src.HashTable import HashTable 
+
 def main():
     print("Running Program...")
+    
+    package_data = load_package_data("data/package_data.csv")
+    package_table = HashTable()
+    print(f"Loaded {len(package_data)} package records from the CSV file.")
+    #print(type(package_data[0][0]))
 
+    for record in package_data:
+        package_table.insert(record)
+    print("Package data inserted into hash table.")
+   
+    
 if __name__ == "__main__":
     main()
--- src/data_loader.py
+++ src/data_loader.py
@@ -4,7 +4,9 @@ def load_package_data(file_path):
     package_data = []
     with open(file_path, newline='') as file:
         reader = csv.reader(file)
+        next(reader)  # Skip the header row
         for row in reader:
+            row[0] = int(row[0])  # Convert the first element (ID) to an integer
             package_data.append(tuple(row))
     return package_data
 
